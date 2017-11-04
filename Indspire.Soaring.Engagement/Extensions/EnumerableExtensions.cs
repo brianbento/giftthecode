f54171d06f55d38d3c840af0d6edcb56dde4b0dd
Adriano Ueda <adriano.ueda@gmail.com>
2017-11-04 17:22:55 -0400

    Started basic pagination.

---
+++ Indspire.Soaring.Engagement/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Indspire.Soaring.Engagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Indspire.Soaring.Engagement.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static PagedList<TList> ToPagedList<TList>(
+            this IEnumerable<TList> list,
+            int totalCount,
+            int page,
+            int pageSize)
+        {
+            return new PagedList<TList>(list)
+            {
+                TotalCount = totalCount
+            };
+        }
+    }
+}
---
+++ Indspire.Soaring.Engagement/Models/IPagedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Indspire.Soaring.Engagement.Models
+{
+    public class PagedList<TList> : IEnumerable<TList>
+    {
+        public int TotalCount { get; set; }
+
+        public IEnumerable<TList> List { get; set; }
+
+        public PagedList(IEnumerable<TList> list)
+        {
+            this.List = list;
+        }
+
+        public IEnumerator<TList> GetEnumerator()
+        {
+            return List.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
