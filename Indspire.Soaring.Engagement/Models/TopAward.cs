122b77e5fb8c313076f8fb974a5a18309a1456b8
Adriano Ueda <adriano.ueda@gmail.com>
2017-11-09 23:16:39 -0500

    Started the dashboard.

---
+++ Indspire.Soaring.Engagement/Models/TopAward.cs
@@ -0,0 +1,17 @@
+﻿using Indspire.Soaring.Engagement.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Indspire.Soaring.Engagement.Models
+{
+    public class TopAward
+    {
+        public int AwardID { get; set; }
+
+        public Award Award { get; set; }
+
+        public int TotalPoints { get; set; }
+    }
+}
