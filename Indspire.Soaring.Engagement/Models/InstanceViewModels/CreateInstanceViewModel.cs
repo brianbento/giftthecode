1ac52041b3ef40a58f1d0af92747bd5df20e5b52
Adriano Ueda <adriano.ueda@gmail.com>
2017-12-02 15:41:29 -0500

    Create instance fixes.

--- Indspire.Soaring.Engagement/Models/InstanceViewModels/CreateInstanceViewModel.cs
+++ Indspire.Soaring.Engagement/Models/InstanceViewModels/CreateInstanceViewModel.cs
@@ -1,15 +1,19 @@
 ﻿namespace Indspire.Soaring.Engagement.Models.InstanceViewModels
 {
     using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
 
     public class CreateInstanceViewModel
     {
+        [Required]
         [DisplayName("Event Name")]
         public string Name { get; set; }
 
+        [Required]
         [DisplayName("Description")]
         public string Description { get; set; }
 
+        [Required]
         [DisplayName("Default Instance")]
         public bool DefaultInstance { get; set; }
     }
