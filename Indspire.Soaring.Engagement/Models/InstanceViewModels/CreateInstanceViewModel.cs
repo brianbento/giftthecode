367007dcf9a04b9c77d660a26dcbf47e454e4d18
Adriano Ueda <adriano.ueda@gmail.com>
2017-12-02 14:57:41 -0500

    Added the create/edit actions for the event.

---
+++ Indspire.Soaring.Engagement/Models/InstanceViewModels/CreateInstanceViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Indspire.Soaring.Engagement.Models.InstanceViewModels
+{
+    using System.ComponentModel;
+
+    public class CreateInstanceViewModel
+    {
+        [DisplayName("Event Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Default Instance")]
+        public bool DefaultInstance { get; set; }
+    }
+}
---
+++ Indspire.Soaring.Engagement/Models/InstanceViewModels/EditInstanceViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Indspire.Soaring.Engagement.Models.InstanceViewModels
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    public class EditInstanceViewModel
+    {
+        [Required]
+        public int InstanceID { get; set; }
+
+        [Required]
+        [DisplayName("Event Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [DisplayName("Default Instance")]
+        public bool DefaultInstance { get; set; }
+    }
+}
