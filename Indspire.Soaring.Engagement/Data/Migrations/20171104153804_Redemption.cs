7300a731b2e1d4305790db8bac475c8753a182a7
Adriano Ueda <adriano.ueda@gmail.com>
2017-12-03 11:25:17 -0500

    Fixed the previous commented out migration.

--- Indspire.Soaring.Engagement/Data/Migrations/20171104153804_Redemption.cs
+++ Indspire.Soaring.Engagement/Data/Migrations/20171104153804_Redemption.cs
@@ -9,17 +9,17 @@ namespace Indspire.Soaring.Engagement.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.DropIndex(
-            //    name: "UserNameIndex",
-            //    table: "AspNetUsers");
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
 
-            //migrationBuilder.DropIndex(
-            //    name: "IX_AspNetUserRoles_UserId",
-            //    table: "AspNetUserRoles");
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
 
-            //migrationBuilder.DropIndex(
-            //    name: "RoleNameIndex",
-            //    table: "AspNetRoles");
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
 
             migrationBuilder.CreateTable(
                 name: "Redemption",
@@ -39,27 +39,27 @@ namespace Indspire.Soaring.Engagement.Data.Migrations
                     table.PrimaryKey("PK_Redemption", x => x.RedemptionID);
                 });
 
-            //migrationBuilder.CreateIndex(
-            //    name: "UserNameIndex",
-            //    table: "AspNetUsers",
-            //    column: "NormalizedUserName",
-            //    unique: true,
-            //    filter: "[NormalizedUserName] IS NOT NULL");
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
 
-            //migrationBuilder.CreateIndex(
-            //    name: "RoleNameIndex",
-            //    table: "AspNetRoles",
-            //    column: "NormalizedName",
-            //    unique: true,
-            //    filter: "[NormalizedName] IS NOT NULL");
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
 
-            //migrationBuilder.AddForeignKey(
-            //    name: "FK_AspNetUserTokens_AspNetUsers_UserId",
-            //    table: "AspNetUserTokens",
-            //    column: "UserId",
-            //    principalTable: "AspNetUsers",
-            //    principalColumn: "Id",
-            //    onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
