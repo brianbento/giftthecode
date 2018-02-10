a271fa8f98c883056f97d26a75040c9c078674a8
James Vidler <james.vidler@outlook.com>
2018-02-10 17:11:49 -0500

    Added application insights

--- Indspire.Soaring.Engagement/Program.cs
+++ Indspire.Soaring.Engagement/Program.cs
@@ -19,6 +19,7 @@ namespace Indspire.Soaring.Engagement
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
